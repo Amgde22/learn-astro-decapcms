5385568957b2c3af28ed724f711b4199f044320a
Amgde22 <magidembarek@gmail.com>
2025-11-13 21:41:26 +0100

    add shadows , icons , darker hero image + json for ai context

--- src/components/AmButton.vue
+++ src/components/AmButton.vue
@@ -58,7 +58,7 @@ const props = defineProps({
   justify-content: center;
   isolation: isolate;
   gap: 0.25em;
-
+  box-shadow:  0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1) ;
   &::before {
     content: '';
     position: absolute;
