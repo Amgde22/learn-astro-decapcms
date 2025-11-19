df8ddec8db7d2d744b7bea361cec7b2ea53a9050
Amgde22 <magidembarek@gmail.com>
2025-11-19 21:27:26 +0100

    add internalization with astrolicious/i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,37 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import vue from "@astrojs/vue";
 
+import i18n from "@astrolicious/i18n";
 
 
 
 export default defineConfig({
-  // update me!
+// update me!
   site: "https://www.yourwebsite.com",
 
-  integrations: [icon(), sitemap({
+  integrations: [icon(),
+   sitemap({    
+      i18n: {
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `"https://www.yourwebsite.com/"` will be treated as default locale, i.e. `en`
+        locales: {
+          // key/value pairs of all languages supported
+          en: 'en-US', // The `defaultLocale` value must be present in `locales` keys
+          fr: 'fr-FR',
+        },
+      },
     filter: (page) => !page.includes("/admin"),
     changefreq: "weekly",
     priority: 0.7,
-  }), vue({
+   }),
+   i18n({
+    defaultLocale: "en",
+    locales: ["fr", "en"],
+    client: {
+      data: true,
+      paths: true,
+    },
+  }), 
+  vue({
     template: {
       compilerOptions: {
         isCustomElement: (tag) => tag.includes('-'),
